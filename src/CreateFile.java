c54a31b16a3501016dad279b0b298ac04a336fe6
Admin <alexfonsid@gmail.com>
2022-07-25 08:08:08 +0300

    createFile

---
+++ src/CreateFile.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CreateFile {
+    public static void createFile() {
+        try {
+            File file = new File("departments from file.txt");
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            PrintWriter printWriter = new PrintWriter(file);
+            printWriter.println("Hello world");
+            printWriter.close();
+        } catch(IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
