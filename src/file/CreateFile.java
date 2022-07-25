58ff082159420ea9ea17b4a1bf33f991b8c35ae2
Admin <alexfonsid@gmail.com>
2022-07-25 14:27:22 +0300

    ReadFromFile4.java

--- src/file/CreateFile.java
+++ src/file/CreateFile.java
@@ -1,3 +1,5 @@
+package file;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
--- src/file/ReadFromFile.java
+++ src/file/ReadFromFile.java
@@ -1,24 +1,21 @@
 package file;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class ReadFromFile {
-    public static void main(String[] args) throws IOException {
+    public static void readFromFile1() throws IOException {
         String path = "D:\\JAVA\\Data\\departments.txt";
 
         FileInputStream stream = new FileInputStream(path);
         int length = stream.available();
         byte[] data = new byte[length];
-        stream.read(data);
+        int read = stream.read(data);
         String text = new String(data);
         System.out.println(text);
     }
 
-    public static void dataFromFile(File file) {
+    public static void readFromFile2(File file) {
         try {
 
             Scanner scanner = new Scanner(file);
@@ -29,5 +26,32 @@ public class ReadFromFile {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void readFromFile3(String path) throws IOException {
+        Reader reader = new FileReader(path);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        int i = 0;
+//        while ((i = reader.read()) != -1) {
+//            System.out.print((char) i);
+//        }
+
+        while (bufferedReader.read() != -1) {
+            System.out.println(bufferedReader.readLine());
+        }
+    }
+
+    public static void readFromFile4(String path) throws IOException {
+        InputStream inputStream = new FileInputStream(path);
+
+        int length = inputStream.available();
+        byte[] data = new byte[length];
+        inputStream.read(data);
+
+        String text = new String(data);
+        System.out.println(text);
+
+
     }
 }
